ddf2d5358b4d7a8c11454a499be081982543a78f
Sheb <michael@sheb.rocks>
2023-01-25 14:57:58 +0100

    named vs default import and export

---
+++ helpers/searchFunctions.js
@@ -0,0 +1,8 @@
+function bubbleSort() {}
+
+function quickSort() {}
+
+function timSort() {}
+
+export { bubbleSort, quickSort, timSort };
+//NICHT DASHIER: export default { bubbleSort, quickSort, timSort };
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,20 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useState } from "react";
 
+//named exports brauchen named imports, der name _muss_ gleich sein
+// "as" bennent die importierte variable um
+// braucht named export
+import { quickSort as sort } from "@/helpers/searchFunctions";
+
+// * import holt alle named exports, aber _nicht den default_
+// braucht named export
+import * as searchFunctions from "@/helpers/searchFunctions";
+//searchFunctions.bubbleSort()
+
+//default importe haben keinen namen, können benannt wie man will
+//default import braucht default export
+import Currywurst from "@/components/Layout";
+
 export default function Home() {
   const [input, setInput] = useState("");
 
